32f5d09245821e5844432c09e37297bad2404b5c
RASEL-BABU <baburasel675@gmail.com>
2025-12-27 18:03:48 +0600

    end of the day

--- projects/views.py
+++ projects/views.py
@@ -3,8 +3,37 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+
+
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
 def projects(request):
-    return render (request,"products.html")
+
+    message= "Wow! you are here"
+    Age = 20
+    context={"msg":message,"number":Age,"projects":projectsList}
+    return render (request,"products.html",context)
 
 def project(request,pk):
-    return render(request,"single_products.html")
+    projectsObj=None
+    for i in projectsList:
+        if i['id']==pk:
+            projectsObj=i
+
+    return render(request,"single_products.html",{'project':projectsObj})
